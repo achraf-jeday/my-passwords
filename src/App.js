dfe426082da5c28a9272d5ea176159124bc87b8f
Achraf Jeday <achraf.jday@gmail.com>
2021-12-27 07:14:12 +0100

    Get entry data from API.

--- src/App.js
+++ src/App.js
@@ -104,9 +104,50 @@ function Copyright() {
 }
 
 function AlertDialog() {
+
+  const [name, setName] = React.useState("");
+  const [userId, setUserId] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [link, setLink] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [metatag, setMetatag] = React.useState("");
+  const [notes, setNotes] = React.useState("");
+
   const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
+  const data = {
+      grant_type: "password",
+      client_id: "4520791a-1ce0-4dfe-9f50-a6e7beb6c318",
+      client_secret: "SuperSecret123&",
+      scope: "customer",
+      username: "test1",
+      password: "SuperSecret123&"
+  };
+
+  const handleClickOpen = async () => {
+    try {
+      // Make the first request
+      const firstResponse = await axios.post('http://dev.passwordlocker.loc/oauth/token', querystring.stringify(data));
+      // Get the access token
+      var access_token = firstResponse.data.access_token;
+      const instance = axios.create({
+        baseURL: 'http://dev.passwordlocker.loc/',
+        timeout: 1000,
+        headers: {'Authorization': 'Bearer ' + access_token}
+      });
+      // Make the second request
+      const secondResponse = await instance.get('/api/json/password/password/e53b64be-a1f6-4ebe-8f21-98de63e7d46e');
+      setName(secondResponse.data.data.attributes.name);
+      setUserId(secondResponse.data.data.attributes.field_user_id);
+      setPassword(secondResponse.data.data.attributes.field_password);
+      setLink(secondResponse.data.data.attributes.field_link.uri);
+      setEmail(secondResponse.data.data.attributes.field_email);
+      setMetatag(secondResponse.data.data.attributes.metatag);
+      setNotes(secondResponse.data.data.attributes.field_notes);
+      console.log(secondResponse.data.data.attributes);
+    } catch (error) {
+      console.log(error);
+    }
     setOpen(true);
   };
 
@@ -150,22 +191,22 @@ function AlertDialog() {
             <Grid container direction="row" justifyContent="center" alignItems="flex-start">
               <Grid container item spacing={2} xs={6} direction={"column"} justifyContent="center" alignItems="center">
                 <Grid item style={{ width: '90%' }}>
-                  <TextField fullWidth id="name" label="Name" size="small" />
+                  <TextField fullWidth id="name" label="Name" size="small" value={name} />
                 </Grid>
                 <Grid item style={{ width: '90%' }}>
-                  <TextField fullWidth id="user-id" label="User ID" size="small" />
+                  <TextField fullWidth id="user-id" label="User ID" size="small" value={userId} />
                 </Grid>
                 <Grid item style={{ width: '90%' }}>
-                  <TextField fullWidth id="password" label="Password" size="small" type="password" />
+                  <TextField fullWidth id="password" label="Password" size="small" type="password" value={password} />
                 </Grid>
                 <Grid item style={{ width: '90%' }}>
-                  <TextField fullWidth id="link" label="Link" size="small" />
+                  <TextField fullWidth id="link" label="Link" size="small" value={link} />
                 </Grid>
                 <Grid item style={{ width: '90%' }}>
-                  <TextField fullWidth id="email" label="Email" size="small" />
+                  <TextField fullWidth id="email" label="Email" size="small" value={email} />
                 </Grid>
                 <Grid item style={{ width: '90%' }}>
-                  <TextField fullWidth id="tags" label="Tags" size="small" />
+                  <TextField fullWidth id="tags" label="Tags" size="small" value={metatag} />
                 </Grid>
               </Grid>
               <Grid container item spacing={2} xs={6} direction={"column"} justifyContent="center" alignItems="center">
@@ -178,6 +219,7 @@ function AlertDialog() {
                     multiline
                     rows={8}
                     maxRows={10}
+                    value={notes}
                   />
                 </Grid>
               </Grid>
