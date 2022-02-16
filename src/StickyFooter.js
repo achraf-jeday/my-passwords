fefbe63dd743c5b1bb4d6c9b4ca5ecd69a30f251
Achraf JEDAY <achraf.jeday@gmail.com>
2022-02-16 20:35:07 +0000

    Fix base URL error and update the .gitignore file.

--- src/StickyFooter.js
+++ src/StickyFooter.js
@@ -7,7 +7,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
       {'Copyright © '}
-      <Link color="inherit" href="https://www." + process.env.REACT_APP_BASE_URL + "/">
+      <Link color="inherit" href={"https://www." + process.env.REACT_APP_BASE_URL + "/"}>
         Password Locker
       </Link>{' '}
       {new Date().getFullYear()}
