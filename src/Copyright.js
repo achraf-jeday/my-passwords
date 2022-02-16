f6ce8672b8fcfee7403caad49e4c06e03ce666dd
Achraf Jeday <achraf.jday@gmail.com>
2022-02-16 21:10:49 +0100

    Add file '.env.example'.

--- src/Copyright.js
+++ src/Copyright.js
@@ -5,7 +5,7 @@ export default function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://www.passwordlocker.me/">
+      <Link color="inherit" href="https://www." + process.env.REACT_APP_BASE_URL + "/">
         Password Locker
       </Link>{' '}
       {new Date().getFullYear()}
